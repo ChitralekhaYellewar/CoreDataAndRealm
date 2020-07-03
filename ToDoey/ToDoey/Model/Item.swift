32283296e9a81258ecfcc1ed529cc84b10e8bd39
chitralekha-yellewar <chitralekha.yellewar@gmail.com>
2020-07-03 16:56:35 +0530

    Add Item button implementation

---
+++ ToDoey/ToDoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Chitralekha Yellewar on 03/07/20.
+//  Copyright © 2020 Chitralekha Yellewar. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var name: String?
+    var check: Bool = false
+}
--- ToDoey/ToDoey/ToDoeyViewController.swift
+++ ToDoey/ToDoey/ToDoeyViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ToDoeyViewController: UITableViewController {
     
-    var itemArray = ["tea", "coffee", "breakfast", "lunch"]
+    var itemArray: [Item] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tea = Item()
+        tea.name = "tea"
+        tea.check = false
+        itemArray.append(tea)
+        itemArray.append(tea)
+        itemArray.append(tea)
+        itemArray.append(tea)
+        itemArray.append(tea)
+        itemArray.append(tea)
+        itemArray.append(tea)
     }
 
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        var itemTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            itemTextField = textField
+        }
+        
+        let alertAction = UIAlertAction(title: "Add", style: .default) { addItem in
+            print(itemTextField.text)
+        }
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 extension ToDoeyViewController {
@@ -27,14 +52,21 @@ extension ToDoeyViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let itemCell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        itemCell.textLabel?.text = itemArray[indexPath.row]
+        itemCell.textLabel?.text = itemArray[indexPath.row].name
+        itemCell.accessoryType = itemArray[indexPath.row].check ? .checkmark : .none
         return itemCell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let itemCell = tableView.cellForRow(at: indexPath)
-        itemCell?.accessoryType = .checkmark
+        itemArray[indexPath.row].check = !itemArray[indexPath.row].check
+
+        itemCell?.accessoryType = itemArray[indexPath.row].check ? .none : .checkmark
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
 }
